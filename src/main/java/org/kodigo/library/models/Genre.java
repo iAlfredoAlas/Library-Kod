3a75988bebac1a4d514fd60e949b95545e0b2dbb
Alfredo Alas <alfredo.alas14@gmail.com>
2023-07-16 15:49:07 -0600

    Adds class Genre

---
+++ src/main/java/org/kodigo/library/models/Genre.java
@@ -0,0 +1,38 @@
+package org.kodigo.library.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table (name = "genre")
+@SQLDelete(sql = "UPDATE genre SET status_genre = false WHERE id_genre=?")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Genre implements Serializable {
+    private static final long serialVersionUID = 123456789L;
+
+    @Id
+    @Basic(optional = false)
+    @Column(name = "id_genre")
+    @Getter
+    @Setter
+    private Long idGenre;
+
+    @Basic(optional = false)
+    @Column(name = "name_genre")
+    @Getter
+    @Setter
+    private String nameGenre;
+
+    @Basic(optional = false)
+    @Column(name = "estatus_genre")
+    @Getter
+    @Setter
+    private Boolean isActiveGenre = Boolean.TRUE;
+}
