f3f432b635a9521d3277ec07402e275f5a7c9afc
Alfredo Alas <alfredo.alas14@gmail.com>
2023-07-16 14:56:45 -0600

    Adds dependencies

---
+++ src/main/java/org/kodigo/library/LibraryApplication.java
@@ -0,0 +1,18 @@
+package org.kodigo.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+}
