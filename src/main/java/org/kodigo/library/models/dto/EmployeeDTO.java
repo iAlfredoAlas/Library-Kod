10618c37b48b0c1f7f64df3f9761a39af7b6da68
Jerson <jerhcarranza@yahoo.com>
2023-07-17 14:52:07 -0600

    Adds EmployeeDTO Class

---
+++ src/main/java/org/kodigo/library/models/dto/EmployeeDTO.java
@@ -0,0 +1,28 @@
+package org.kodigo.library.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeDTO  {
+
+    @Getter
+    @Setter
+    private Long idEmployee;
+
+    @Getter
+    @Setter
+    private String nameEmployee;
+
+    @Getter
+    @Setter
+    private String employeeNumber;
+
+    @Getter
+    @Setter
+    private Boolean isActiveEmployee = Boolean.TRUE;
+}
