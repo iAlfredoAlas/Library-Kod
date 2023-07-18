efcc8877fae6ebfbd583ee16752a2a0b98a047e5
Alfredo Alas <alfredo.alas14@gmail.com>
2023-07-18 15:13:01 -0600

    Adds method findByIsActiveGenre

--- src/main/java/org/kodigo/library/repository/IGenreRepository.java
+++ src/main/java/org/kodigo/library/repository/IGenreRepository.java
@@ -2,6 +2,9 @@ package org.kodigo.library.repository;
 
 import org.kodigo.library.models.Genre;
 
+import java.util.List;
+
 public interface IGenreRepository extends IGenericRepository<Genre, Long> {
 
+    List<Genre> findByIsActiveGenre(Boolean isActiveGenre);
 }
