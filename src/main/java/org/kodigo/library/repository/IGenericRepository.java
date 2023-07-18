73ea360ce85af27cbe7cb15a6274436217bbcd2d
Alfredo Alas <alfredo.alas14@gmail.com>
2023-07-18 15:12:34 -0600

    Adds @NoRepositoryBean and delete method findByIsActive

--- src/main/java/org/kodigo/library/repository/IGenericRepository.java
+++ src/main/java/org/kodigo/library/repository/IGenericRepository.java
@@ -1,11 +1,9 @@
 package org.kodigo.library.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
-import java.util.List;
 
+@NoRepositoryBean
 public interface IGenericRepository<T, ID> extends JpaRepository<T, ID> {
-
-    List<T> findByIsActive(Boolean isActive);
-
 }
