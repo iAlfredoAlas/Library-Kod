1f96833453b56a761d2346d9cf2661fa200af50f
Alfredo Alas <alfredo.alas14@gmail.com>
2023-08-15 09:54:35 -0600

    Refactor name to GenreServiceImplementTest

---
+++ src/test/java/org/kodigo/library/EditorialServiceImplementTest.java
@@ -0,0 +1,23 @@
+package org.kodigo.library;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.kodigo.library.models.Editorial;
+import org.kodigo.library.repository.IEditorialRepository;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EditorialServiceImplementTest {
+}
