1b066ae9af60b520cbb3ae42769c90636c8f5bc8
Alfredo Alas <alfredo.alas14@gmail.com>
2023-08-15 10:15:16 -0600

    Refactor name to BooklServiceImplementTest

---
+++ src/test/java/org/kodigo/library/AuthorServiceImplementTest.java
@@ -0,0 +1,113 @@
+package org.kodigo.library;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.runner.RunWith;
+import org.kodigo.library.service.AuthorServiceImplement;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.kodigo.library.models.Author;
+import org.kodigo.library.repository.IAuthorRepository;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class AuthorServiceImplementTest {
+
+    @Mock
+    private IAuthorRepository authorRepository;
+
+    @InjectMocks
+    private AuthorServiceImplement authorService;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    @Order(1)
+    public void testGetAllAuthors() {
+        List<Author> authors = new ArrayList<>();
+        authors.add(new Author(1L, "Author 1", "Country 1", new Date(), true));
+        authors.add(new Author(2L, "Author 2", "Country 2", new Date(), true));
+
+        when(authorRepository.findAll()).thenReturn(authors);
+
+        List<Author> result = authorService.getAll();
+
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    @Order(2)
+    public void testFindAuthorById_ExistingAuthor() {
+        Author author = new Author(1L, "Author 1", "Country 1", new Date(), true);
+
+        when(authorRepository.findById(1L)).thenReturn(Optional.of(author));
+
+        Author result = authorService.findById(1L);
+
+        assertEquals("Author 1", result.getNameAuthor());
+    }
+
+    @Test
+    @Order(3)
+    public void testFindAuthorById_NonExistingAuthor() {
+        when(authorRepository.findById(1L)).thenReturn(Optional.empty());
+
+        assertThrows(EmptyResultDataAccessException.class, () -> authorService.findById(1L));
+    }
+
+    @Test
+    @Order(4)
+    public void testAddAuthor() {
+        Author author = new Author(1L, "Author 1", "Country 1", new Date(), true);
+
+        when(authorRepository.save(any(Author.class))).thenReturn(author);
+
+        Author result = authorService.add(author);
+
+        assertEquals("Author 1", result.getNameAuthor());
+    }
+
+    @Test
+    @Order(5)
+    public void testUpdateAuthor() {
+        Author existingAuthor = new Author(1L, "Author 1", "Country 1", new Date(), true);
+        Author updatedAuthor = new Author(1L, "Updated Author", "Country 1", new Date(), false);
+
+        when(authorRepository.findById(1L)).thenReturn(Optional.of(existingAuthor));
+        when(authorRepository.save(any(Author.class))).thenReturn(updatedAuthor);
+
+        Author result = authorService.update(updatedAuthor, 1L);
+
+        assertEquals("Updated Author", result.getNameAuthor());
+        assertEquals(false, result.getIsActiveAuthor());
+    }
+
+    @Test
+    @Order(6)
+    public void testDeleteAuthor() {
+        Long authorId = 1L;
+
+        authorService.deleteLog(authorId);
+
+        verify(authorRepository, times(1)).deleteById(authorId);
+    }
+}
+
